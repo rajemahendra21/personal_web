8f29bf8f58bc589c81b45cfef7ef74f2ed877ccb
rajemahendra21 <rajemax21@gmail.com>
2014-01-23 11:29:14 +0530

    change personal web
    change personal web

---
+++ Script/seting/edm-seting.js
@@ -0,0 +1,2 @@
+//seting
+jQuery(window).load(function(){resizeImg();works_resize();$("#MIX").mixitup({effects:["fade"]});$("#content_1").mCustomScrollbar({autoHideScrollbar:!0,theme:"light-thin"});$(window).resize(function(){resizeImg();works_resize()});$(".CLOSE_BTN").click()});
